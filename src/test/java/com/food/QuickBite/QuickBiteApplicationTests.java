20c6446324f7d9cd0192e5961b901738d57c8650
Delta Student <praneeth@email.com>
2026-02-12 19:14:06 +0530

    Initial commit - QuickBite fullstack project

---
+++ src/test/java/com/food/QuickBite/QuickBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.QuickBite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
