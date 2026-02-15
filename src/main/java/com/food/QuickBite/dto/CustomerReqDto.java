91cd135f47ae11ca15e9ab15fddd4dc1344fdd44
Vamshi-Krishna-Reddy <mvamshikrishnareddy469@gmail.com>
2026-02-15 18:03:34 +0530

    added service and controller for customer

--- src/main/java/com/food/QuickBite/dto/CustomerReqDto.java
+++ src/main/java/com/food/QuickBite/dto/CustomerReqDto.java
@@ -12,5 +12,43 @@ public class CustomerReqDto {
 	private String mail;
 	@Enumerated(EnumType.STRING)
 	private Gender gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getMob() {
+		return mob;
+	}
+	public void setMob(long mob) {
+		this.mob = mob;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "CustomerReqDto [name=" + name + ", mob=" + mob + ", mail=" + mail + ", gender=" + gender + "]";
+	}
+	public CustomerReqDto(String name, long mob, String mail, Gender gender) {
+		super();
+		this.name = name;
+		this.mob = mob;
+		this.mail = mail;
+		this.gender = gender;
+	}
+	public CustomerReqDto() {
+		super();
+	}
 	
 }
