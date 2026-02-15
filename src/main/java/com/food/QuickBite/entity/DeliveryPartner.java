d8fd1f525c33fa72eb04e4ffd369faa64e344cd5
Delta Student <praneeth@email.com>
2026-02-15 16:36:40 +0530

    added

--- src/main/java/com/food/QuickBite/entity/DeliveryPartner.java
+++ src/main/java/com/food/QuickBite/entity/DeliveryPartner.java
@@ -2,11 +2,10 @@ package com.food.QuickBite.entity;
 
 import java.util.List;
 
-<<<<<<< HEAD
+
 import com.food.QuickBite.enums.DeliveryStatus;
 
-=======
->>>>>>> 1f85e587c5a014d4157d0db7bdc3f04563365668
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -27,12 +26,10 @@ public class DeliveryPartner {
 	private Address address;
 	private List<Order> orders;
 	private String vehicle;
-<<<<<<< HEAD
+
 	@Enumerated(EnumType.STRING)
 	private DeliveryStatus deliveryStatus;
-=======
+
 	//private PartnerStatus status; 
->>>>>>> 1f85e587c5a014d4157d0db7bdc3f04563365668
-	
 
 }
